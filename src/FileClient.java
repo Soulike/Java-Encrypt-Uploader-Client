3e653eb41687a34c731b59b330bcbc55d95d3b10
Soulike <316413310@qq.com>
2018-07-14 19:43:14 +0800

    代码完成

---
+++ src/FileClient.java
@@ -0,0 +1,149 @@
+import util.Logger.MyLogger;
+import util.Objects.Message;
+import util.Objects.UploadFileInfo;
+
+import javax.crypto.Cipher;
+import javax.crypto.CipherOutputStream;
+import javax.crypto.NoSuchPaddingException;
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+
+import static util.Compressor.Compressor.*;
+
+import java.io.*;
+import java.net.Socket;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.security.InvalidAlgorithmParameterException;
+import java.security.InvalidKeyException;
+import java.security.Key;
+import java.security.NoSuchAlgorithmException;
+
+public class FileClient
+{
+    private static String ENCRYPT_MODE = "AES/CFB8/NoPadding";
+    private static MyLogger logger = new MyLogger("文件上传");
+
+    public static void main(String[] args)
+    {
+        if (args.length != 3)
+        {
+            System.out.println("参数数量错误，请输入正确参数。例如: java FileClient 1.2.3.4 6666 /home/java/");
+        }
+        else
+        {
+            Path filePath = Paths.get(args[2]);
+            // 如果是目录，就需要打包
+            try
+            {
+                if (filePath.toFile().isDirectory())
+                {
+                    try
+                    {
+                        Path zippedFilePath = Files.createTempFile(filePath.toFile().getName(), "zip");
+                        compress(filePath, zippedFilePath);
+                        uploadFile(args[0], Integer.parseInt(args[1]), zippedFilePath, true);
+                    }
+                    catch (IOException e)
+                    {
+                        System.out.println("创建临时文件失败");
+                        e.printStackTrace();
+                    }
+
+                }
+                // 如果不是目录，直接上传
+                else if (filePath.toFile().isFile())
+                {
+                    uploadFile(args[0], Integer.parseInt(args[1]), filePath, false);
+                }
+            }
+            catch (IOException e)
+            {
+                logger.logError("与服务器的连接失败");
+                e.printStackTrace();
+            }
+            catch (ClassNotFoundException e)
+            {
+                logger.logError("服务器返回异常");
+                e.printStackTrace();
+            }
+            catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException | InvalidAlgorithmParameterException e)
+            {
+                logger.logError("加密出现问题");
+                e.printStackTrace();
+            }
+        }
+    }
+
+
+    private static void uploadFile(String address, int port, Path filePath, boolean isZipped) throws IOException, ClassNotFoundException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, InvalidAlgorithmParameterException
+    {
+        Cipher cipher;
+        Key key = getKey("JavaUploader");
+        cipher = Cipher.getInstance(ENCRYPT_MODE);
+        IvParameterSpec iv = new IvParameterSpec("1122334455667788".getBytes());
+        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+
+        Socket socket = new Socket(address, port);
+        InputStream rawIn = socket.getInputStream();
+        OutputStream rawOut = socket.getOutputStream();
+        CipherOutputStream out = new CipherOutputStream(rawOut, cipher);
+
+        ObjectOutputStream objOut = new ObjectOutputStream(out);
+
+        File file = filePath.toFile();
+        UploadFileInfo fileInfo = new UploadFileInfo(file.getName(), file.length(), isZipped);
+        objOut.writeObject(fileInfo);
+        objOut.flush();
+
+        DataOutputStream dataOut = new DataOutputStream(out);
+        DataInputStream fileDataIn = new DataInputStream(new FileInputStream(filePath.toFile()));
+        byte[] buffer = new byte[256];
+        int readBytes = 0;
+        while ((readBytes = fileDataIn.read(buffer)) != -1)
+        {
+            dataOut.write(buffer, 0, readBytes);
+            dataOut.flush();
+        }
+        fileDataIn.close();
+        socket.shutdownOutput();
+
+        ObjectInputStream dataIn = new ObjectInputStream(rawIn);
+        Message message = (Message) dataIn.readObject();
+        if (message.isSuccessful())
+        {
+            logger.logInfo(message.getMessage());
+        }
+        else
+        {
+            logger.logError(message.getMessage());
+        }
+        socket.close();
+    }
+
+    private static Key getKey(String password)
+    {
+        byte[] ketData = get16BytesKetData(password);
+        return new SecretKeySpec(ketData, "AES");
+    }
+
+    private static byte[] get16BytesKetData(String password)
+    {
+        byte[] passwordBytes = password.getBytes(StandardCharsets.UTF_8);
+        byte[] BytesKeyData = new byte[16];
+        int copyLength = passwordBytes.length >= 16 ? 16 : passwordBytes.length;
+        for (int i = 0; i < copyLength; i++)
+        {
+            BytesKeyData[i] = passwordBytes[i];
+        }
+
+        for (int i = copyLength; i < 16; i++)
+        {
+            BytesKeyData[i] = 0;
+        }
+
+        return BytesKeyData;
+    }
+}
---
+++ src/util/Compressor/Compressor.java
@@ -0,0 +1,133 @@
+package util.Compressor;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.zip.CRC32;
+import java.util.zip.CheckedOutputStream;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipOutputStream;
+
+/**
+ * 压缩文件或文件夹到 Zip 文件。
+ *
+ * @author soulike
+ */
+public class Compressor
+{
+    /**
+     * 通过命令行参数可将指定文件压缩为 Zip 文件，存放在当前目录。
+     */
+    public static void main(String[] args)
+    {
+        if (args.length != 1)
+        {
+            System.out.println("参数数量错误");
+        }
+        else
+        {
+            Path path = Paths.get(args[0]);
+            try
+            {
+                compress(path, Paths.get("./", String.format("%s.zip", path.getFileName().toString())));
+            }
+            catch (IOException e)
+            {
+                System.out.println("压缩失败");
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 压缩一个文件/文件夹成为一个 Zip 文件。
+     *
+     * @param srcPath 要被压缩的文件/文件夹 Path 对象。
+     * @param dstPath 被压缩文件 Path 对象。
+     */
+    public static void compress(Path srcPath, Path dstPath) throws IOException
+    {
+        if (Files.notExists(srcPath))
+        {
+            throw new FileNotFoundException(String.format("%s 不存在", srcPath.toString()));
+        }
+
+        if (Files.exists(dstPath))
+        {
+            Files.delete(dstPath);
+        }
+
+        Files.createDirectories(dstPath.getParent());
+        Files.createFile(dstPath);
+
+        try (FileOutputStream out = new FileOutputStream(dstPath.toFile());
+             CheckedOutputStream cos = new CheckedOutputStream(out, new CRC32());
+             ZipOutputStream zipOut = new ZipOutputStream(cos, StandardCharsets.UTF_8))
+        {
+            Path basePath = Paths.get("");
+            compress(srcPath.toFile(), zipOut, basePath);
+        }
+    }
+
+    private static void compress(File file, ZipOutputStream zipOut, Path basePath) throws IOException
+    {
+        if (file.isDirectory())
+        {
+            compressDirectory(file, zipOut, basePath);
+        }
+        else
+        {
+            compressFile(file, zipOut, basePath);
+        }
+    }
+
+    /**
+     * 压缩一个文件夹。
+     *
+     * @param dir      文件夹 File 对象。
+     * @param zipOut   指向 Zip 文件的输出流。
+     * @param basePath dir 所在的目录。
+     */
+    private static void compressDirectory(File dir, ZipOutputStream zipOut, Path basePath) throws IOException
+    {
+        Object[] fileList = Files.list(dir.toPath()).toArray();
+        File file;
+        ZipEntry entry = new ZipEntry(dir.toString() + "/");
+        zipOut.putNextEntry(entry);
+
+        for (Object obj : fileList)
+        {
+            file = ((Path) obj).toFile();
+            compress(file, zipOut, Paths.get(basePath.toString(), dir.getName()));
+        }
+    }
+
+    /**
+     * 压缩一个文件。
+     *
+     * @param file     文件 File 对象。
+     * @param zipOut   指向 Zip 文件的输出流。
+     * @param basePath file 所在的目录。
+     */
+    private static void compressFile(File file, ZipOutputStream zipOut, Path basePath) throws IOException
+    {
+        if (Files.notExists(file.toPath()))
+        {
+            return;
+        }
+
+        try (DataInputStream in = new DataInputStream(new FileInputStream(file)))
+        {
+            ZipEntry entry = new ZipEntry(Paths.get(basePath.toString(), file.getName()).toString());
+            zipOut.putNextEntry(entry);
+            int readBytes = 0;
+            byte data[] = new byte[1024 * 1024];
+            while ((readBytes = in.read(data)) != -1)
+            {
+                zipOut.write(data, 0, readBytes);
+            }
+        }
+    }
+}
