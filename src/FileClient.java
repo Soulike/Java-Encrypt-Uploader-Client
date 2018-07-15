1c1a78135c8f69f394b8ddff19e5b8f571ba06da
Soulike <316413310@qq.com>
2018-07-15 15:32:22 +0800

    基本完成

--- src/FileClient.java
+++ src/FileClient.java
@@ -2,13 +2,15 @@ import util.Logger.MyLogger;
 import util.Objects.Message;
 import util.Objects.UploadFileInfo;
 
+import static util.FileTool.FileStructureReader.*;
+
 import javax.crypto.Cipher;
+import javax.crypto.CipherInputStream;
 import javax.crypto.CipherOutputStream;
 import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-import static util.Compressor.Compressor.*;
 
 import java.io.*;
 import java.net.Socket;
@@ -20,11 +22,14 @@ import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.util.List;
 
 public class FileClient
 {
     private static String ENCRYPT_MODE = "AES/CFB8/NoPadding";
     private static MyLogger logger = new MyLogger("文件上传");
+    private static Cipher outCipher;
+    private static Cipher inCipher;
 
     public static void main(String[] args)
     {
@@ -34,33 +39,12 @@ public class FileClient
         }
         else
         {
-            Path filePath = Paths.get(args[2]);
-            // 如果是目录，就需要打包
+            final Path filePath = Paths.get(args[2]);
             try
             {
-                // TODO: 文件夹打包方法有问题
-                if (filePath.toFile().isDirectory())
-                {
-                    try
-                    {
-                        Path zippedFilePath = Files.createTempFile(filePath.toFile().getName(), ".zip");
-                        compress(filePath, zippedFilePath);
-                        uploadFile(args[0], Integer.parseInt(args[1]), zippedFilePath, true);
-                        Files.delete(zippedFilePath);
-                    }
-                    catch (IOException e)
-                    {
-                        System.out.println("创建临时文件失败");
-                        e.printStackTrace();
-                    }
-
-                }
-                // 如果不是目录，直接上传
-                else if (filePath.toFile().isFile())
-                {
-                    logger.logInfo("开始上传");
-                    uploadFile(args[0], Integer.parseInt(args[1]), filePath, false);
-                }
+                final Socket socket = connectServer(args[0], Integer.parseInt(args[1]));
+                uploadFile(filePath, socket);
+                socket.close();
             }
             catch (IOException e)
             {
@@ -81,40 +65,76 @@ public class FileClient
     }
 
 
-    private static void uploadFile(String address, int port, Path filePath, boolean isZipped) throws IOException, ClassNotFoundException, NoSuchPaddingException, NoSuchAlgorithmException, InvalidKeyException, InvalidAlgorithmParameterException
+    /**
+     * 上传指定文件/文件夹。如果是文件夹将会递归所有结构后上传。
+     *
+     * @param filePath 要被上传的文件/文件夹路径
+     * @param socket   与服务器建立的 Socket 对象
+     */
+    private static void uploadFile(Path filePath, Socket socket) throws IOException, ClassNotFoundException
     {
-        Cipher cipher;
-        Key key = getKey("JavaUploader");
-        cipher = Cipher.getInstance(ENCRYPT_MODE);
-        IvParameterSpec iv = new IvParameterSpec("1122334455667788".getBytes());
-        cipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        // 记录时间戳，用于计算时间以及上传速度
 
-        Socket socket = new Socket(address, port);
-        InputStream rawIn = socket.getInputStream();
-        OutputStream rawOut = socket.getOutputStream();
-        CipherOutputStream out = new CipherOutputStream(rawOut, cipher);
+        long startTime = 0;
+        long endTime = 0;
 
-        ObjectOutputStream objOut = new ObjectOutputStream(out);
 
-        File file = filePath.toFile();
-        UploadFileInfo fileInfo = new UploadFileInfo(file.getName(), file.length(), isZipped);
-        objOut.writeObject(fileInfo);
-        objOut.flush();
+        CipherOutputStream encryptedOut = new CipherOutputStream(socket.getOutputStream(), outCipher);
+        CipherInputStream decryptedIn = new CipherInputStream(socket.getInputStream(), inCipher);
 
-        DataOutputStream dataOut = new DataOutputStream(out);
-        DataInputStream fileDataIn = new DataInputStream(new FileInputStream(filePath.toFile()));
-        byte[] buffer = new byte[256];
-        int readBytes = 0;
-        while ((readBytes = fileDataIn.read(buffer)) != -1)
+        ObjectOutputStream objOut = new ObjectOutputStream(encryptedOut);
+
+        DataOutputStream dataOut = new DataOutputStream(encryptedOut);
+        DataInputStream fileIn = null;
+
+
+        byte[] buffer = new byte[512];
+
+        List<Path> fileList = getAllFiles(filePath);
+
+        // 需要得到父目录来保证文件的相对路径正确
+        Path parentPath = filePath.getParent();
+
+        UploadFileInfo currentFileInfo = null;
+        File currentFile = null;
+        for (Path path : fileList)
         {
-            dataOut.write(buffer, 0, readBytes);
-            dataOut.flush();
+            currentFile = path.toFile();
+            // 如果是一个文件，就上传
+            if (currentFile.isFile())
+            {
+                startTime = System.currentTimeMillis();
+
+                currentFileInfo = new UploadFileInfo(currentFile.getName(), currentFile.length(), true, parentPath.toAbsolutePath().relativize(path).toString());
+                fileIn = new DataInputStream(new FileInputStream(currentFile));
+                objOut.writeObject(currentFileInfo);
+                objOut.flush();
+
+                int readBytes = 0;
+                while ((readBytes = fileIn.read(buffer)) != -1)
+                {
+                    dataOut.write(buffer, 0, readBytes);
+                    dataOut.flush();
+                }
+
+                endTime = System.currentTimeMillis();
+
+                double fileSizeInMB = currentFile.length() / 1024f / 1024f;
+                double uploadTimeInSecond = (endTime - startTime) / 1000f == 0 ? 1 : (endTime - startTime) / 1000f;
+                logger.logInfo(String.format("%s %f MB 上传成功，用时 %.2f 秒, 平均上传速度 %.2f KB/S", currentFile.getName(), fileSizeInMB, uploadTimeInSecond, fileSizeInMB * 1000 / uploadTimeInSecond));
+            }
+            else if (currentFile.isDirectory())
+            {
+                currentFileInfo = new UploadFileInfo(currentFile.getName(), 0, false, parentPath.toAbsolutePath().relativize(path).toString());
+                objOut.writeObject(currentFileInfo);
+                objOut.flush();
+            }
         }
-        fileDataIn.close();
         socket.shutdownOutput();
 
-        ObjectInputStream dataIn = new ObjectInputStream(rawIn);
-        Message message = (Message) dataIn.readObject();
+
+        ObjectInputStream objIn = new ObjectInputStream(decryptedIn);
+        Message message = (Message) objIn.readObject();
         if (message.isSuccessful())
         {
             logger.logInfo(message.getMessage());
@@ -123,7 +143,6 @@ public class FileClient
         {
             logger.logError(message.getMessage());
         }
-        socket.close();
     }
 
     private static Key getKey(String password)
@@ -148,5 +167,17 @@ public class FileClient
         }
 
         return BytesKeyData;
+    }
+
+    private static Socket connectServer(String address, int port) throws NoSuchPaddingException, NoSuchAlgorithmException, IOException, InvalidKeyException, InvalidAlgorithmParameterException
+    {
+
+        Key key = getKey("JavaUploader");
+        outCipher = Cipher.getInstance(ENCRYPT_MODE);
+        inCipher = Cipher.getInstance(ENCRYPT_MODE);
+        IvParameterSpec iv = new IvParameterSpec("1122334455667788".getBytes());
+        outCipher.init(Cipher.ENCRYPT_MODE, key, iv);
+        inCipher.init(Cipher.DECRYPT_MODE, key, iv);
+        return new Socket(address, port);
     }
 }
---
+++ src/util/FileTool/FileStructureReader.java
@@ -0,0 +1,45 @@
+package util.FileTool;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileStructureReader
+{
+    /**
+     * 得到指定文件夹下所有的文件以及文件夹的 Path 对象
+     *
+     * @param folderPath 想要得到所有结构的文件夹 Path 对象
+     */
+    public static List<Path> getAllFiles(Path folderPath) throws IOException
+    {
+        ArrayList<Path> fileList = new ArrayList<>();
+        // 如果是目录，就递归
+        if (folderPath.toFile().isDirectory())
+        {
+            getAllFilesRecursive(folderPath, fileList);
+        }
+        // 如果传入的是个文件，就直接返回它自己
+        else
+        {
+            fileList.add(folderPath);
+        }
+        return fileList;
+    }
+
+    private static void getAllFilesRecursive(Path path, List<Path> fileList) throws IOException
+    {
+        fileList.add(path);
+        if (path.toFile().isDirectory())
+        {
+            Object[] list = Files.list(path).toArray();
+            for (Object fileObj : list)
+            {
+                getAllFilesRecursive((Path) fileObj, fileList);
+            }
+        }
+    }
+}
